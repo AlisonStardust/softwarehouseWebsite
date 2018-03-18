fd758560503dde578b457f4832a1ef5b779b85f4
alisonstardust <zakulisami@o2.pl>
2018-03-18 17:05:25 +0100

    updates

---
+++ script.js
@@ -0,0 +1,4 @@
+const categories = document.querySelector('.website_categories');
+const portfolioLink = document.querySelector('.website_categories--portfolio');
+const teamLink = document.querySelector('.website_categories--team');
+const contactLink = document.querySelector('.website_categories--contact');
