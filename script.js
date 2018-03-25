e6503edbeef6986fafb62e0b9119a113b10fba61
alisonstardust <zakulisami@o2.pl>
2018-03-25 08:55:44 +0200

    animations

--- script.js
+++ script.js
@@ -2,13 +2,26 @@ const categories = document.querySelector('.website_categories');
 const portfolioLink = document.querySelector('.website_categories--portfolio');
 const teamLink = document.querySelector('.website_categories--team');
 const contactLink = document.querySelector('.website_categories--contact');
+const paragraph = document.querySelector('.website_introduction--paragraph')
 
-function website() {
-portfolioLink.addEventListener("click", function MoveToElement());
+// $(document).ready(function(){
+// $('website_categories--portfolio').scrollTo();
+// }
+
+function animations() {
+  setTimeout(func1, 1500);
+};
+
+function func1() {
+  paragraph.classList.add('shown');
 }
 
-function MoveToElement() {
-  window.scrollTo(10, 1000); 
-}
+animations();
 
-website();
+// function websiteHandler() {
+//   portfolioLink.addEventListener("click", function MoveToElement() {
+//     alert('dupa')
+//     window.scrollTo(10, 1000); 
+//   }
+// }
+// websiteHandler();
