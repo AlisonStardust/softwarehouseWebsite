8da8aee854e324dd02b9d23a86e41f68331cb1dc
alisonstardust <zakulisami@o2.pl>
2018-03-25 08:22:01 +0200

    pdates

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const categories = document.querySelector('.website_categories');
 const portfolioLink = document.querySelector('.website_categories--portfolio');
 const teamLink = document.querySelector('.website_categories--team');
 const contactLink = document.querySelector('.website_categories--contact');
+
+function website() {
+portfolioLink.addEventListener("click", function MoveToElement());
+}
+
+function MoveToElement() {
+  window.scrollTo(10, 1000); 
+}
+
+website();
