ee861c8d36cf05ca89e128fd989e45d984f2eba6
alisonstardust <zakulisami@o2.pl>
2018-03-31 23:03:35 +0200

    scrolling to element added

--- script.js
+++ script.js
@@ -3,28 +3,29 @@ const portfolioLink = document.querySelector('.website_categories--portfolio');
 const teamLink = document.querySelector('.website_categories--team');
 const contactLink = document.querySelector('.website_categories--contact');
 const paragraph = document.querySelector('.website_introduction--paragraph')
-const portoflio = document.querySelector('.website_portfolio')
+const portfolio = document.querySelector('.website_portfolio')
 const team = document.querySelector('.website_team')
 const contact = document.querySelector('.website_contact')
 
+const moveToPortfolio = () => moveToElement(portfolio);
+const moveToTeam = () => moveToElement(team);
+const moveToContact = () => moveToElement(contact);
+
 function wholeWebsite() {
   showDescription();
+  portfolioLink.addEventListener("click", moveToPortfolio);
+  teamLink.addEventListener("click", moveToTeam);
+  contactLink.addEventListener("click", moveToContact);
 }
 
 function showDescription() {
   setTimeout(() => paragraph.classList.add('shown'), 1500)
 };
 
-//scrolling function
-
-portfolioLink.addEventListener("click", function MoveToPortfolio() {
-  portoflio.scrollIntoView({ behavior: 'smooth' }); 
-});
-teamLink.addEventListener("click", function MoveToTeam() {
-  team.scrollIntoView({ behavior: 'smooth' }); 
-});
-contactLink.addEventListener("click", function MoveToContact() {
-  contact.scrollIntoView({ behavior: 'smooth' });  
-});
+function moveToElement(where) {
+  where.scrollIntoView({
+    behavior: 'smooth'
+  });
+}
 
 wholeWebsite();
